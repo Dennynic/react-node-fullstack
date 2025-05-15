60b347e1232bfd831c104a0dc0a74ed395502196
Dennynic <joragdetibil@gmail.com>
2025-05-15 19:00:17 +0500

    Added classic form

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,18 @@ import {trpcRouter} from './trpc';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import cors from 'cors';
 
+const PORT = 3005;
+
 const expressApp =  express();
 expressApp.use(cors());
-expressApp.get("/ping", (req, res) => { 
-    res.send('pong');
-});
+expressApp.get("/ping", (req, res) => {
+    console.log("Запрос получен!");  
+    res.status(200).json({ message: "pong" });  
+  });
 expressApp.use('/trpc', trpcExpress.createExpressMiddleware({
     router: trpcRouter
 }))
 
-expressApp.listen(3000, ()=>{
-    console.info("Server start at http://localhost:3000")
+expressApp.listen(PORT, ()=>{
+    console.info("Server start at http://localhost:"+PORT)
 })
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+import { Segment } from '../../components/Segment';
+import { Input } from '../../components/Input';
+import { TextArea } from '../../components/Textarea';
+
 import css from './index.module.scss';
 
-export const NewIdeaPage = () =>{
-    return (
-        <div>
-            <h1 className={css.title}>New Idea</h1>
-        </div>
-    )
-}
+export const NewIdeaPage = () => {
+  const [state, setState] = useState({
+    name: '',
+    nick: '',
+    description: '',
+    text: '',
+  });
+
+  return (
+    <Segment title="New Idea">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          console.info('Submitted', state);
+        }}
+      >
+        <Input label="Name" name="name" state={state} setState={setState} />
+        <Input label="Nick" name="nick" state={state} setState={setState} />
+        <Input
+          label="Description"
+          name="description"
+          state={state}
+          setState={setState}
+        />
+        <TextArea name="text" state={state} setState={setState} />
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
+  );
+};
